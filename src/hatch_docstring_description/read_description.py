c9b91c04783c91586e32272267d92cd85c469239
Philipp A <flying-sheep@web.de>
2023-06-26 10:18:24 +0200

    wip

---
+++ src/hatch_docstring_description/read_description.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+def read_description(root: Path) -> str:
+    """Returns the first sentence of the docstring."""
+    return ""
--- src/hatch_docstring_description/read_description_hook.py
+++ src/hatch_docstring_description/read_description_hook.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+from typing import Any
+
 from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+from .read_description import read_description
 
 
 class ReadDescriptionHook(MetadataHookInterface):
     """Metadata hook reading the first sentence of the docstring into `description`."""
 
-    PLUGIN_NAME = "custom"
+    PLUGIN_NAME = "docstring-description"
 
-    def update(self, metadata):
+    def update(self, metadata: dict[str, Any]) -> None:
         """See https://ofek.dev/hatch/latest/plugins/metadata-hook/ for more information."""
 
-        metadata["description"] = read_description()
+        path = Path(self.config.get("path", path))
+        metadata["description"] = read_description(Path(self.root) / path)
